6998eb4b20f6b64a1e98cfd4914da647b1f9ea45
Zosia <zofiatokaj@gmail.com>
2021-11-07 20:28:33 +0100

    Fix deployment script.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     assert calculate_average(1.0, 3.0) == 2.0
     assert calculate_average(1.0, 1.0) == 1.0
     assert calculate_average(2.0, 4.0) == 3.0
+    assert calculate_average(2.0, 6.0) == 4.0
     assert calculate_average(1.0, 2.0, 4.23, 2.34) == 2.3925
     assert variance(1.0, 2.0) == 0.25
     assert median(1.0, 4.0, 2.0) == 2.0
