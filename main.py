a7d36cb816d4f8db7ca448fbba325cade1ebf564
Zosia <zofiatokaj@gmail.com>
2021-11-07 16:44:59 +0100

    Add workflow.

---
+++ main.py
@@ -0,0 +1,8 @@
+def sum_two_number(a: int, b: int) -> int:
+    return a + b
+
+
+if __name__ == "__main__":
+    assert sum_two_number(3, 4) == 7
+    assert sum_two_number(5, 6) == 11
+    print("Script finished")
